b123eafd5df2c70c981f312e6b051c65ad0f1232
Zach Klabunde <zbunde@gmail.com>
2014-03-26 16:54:05 -0600

    created passing test for adding new task

--- app.rb
+++ app.rb
@@ -1,10 +1,27 @@
 require 'sinatra/base'
 
 class App <Sinatra::Base
-  get '/' do
-    erb :index
 
+  TASKS = []
+
+  get '/' do
+    erb :index, :locals => {:tasks => TASKS}
+  end
+
+  post '/' do
+    TASKS << params[:task]
+    erb :index, :locals => {:tasks => TASKS}
+  end
+
+  get '/tasks' do
+    erb :tasks
+  end
+
+  get '/tasks/:id' do
 
   end
+
+
+
 end
 
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -8,8 +8,12 @@ feature "interacting with tasks" do
   scenario "user can interact with tasks" do
 
     visit '/'
-    expect(page).to have_content "FOOD"
+    expect(page).to have_content "Welcome"
+    click_link "Add a Task"
 
+    fill_in "task", with: "FOOFIGHTERS"
+    click_on "Create task"
+    expect(page).to have_content "FOOFIGHTERS"
 
   end
 
